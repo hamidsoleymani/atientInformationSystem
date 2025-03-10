42a41c2c8110cf7c158d0fa6229b33512edd9396
hamid soleymani <hamid.soleymani@gmx.de>
2025-03-10 21:36:38 +0100

    jenkins 5

--- src/main/java/com/boostmedia/patientinformationsystem/FlywayCleanMigrateRunner.java
+++ src/main/java/com/boostmedia/patientinformationsystem/FlywayCleanMigrateRunner.java
@@ -16,7 +16,7 @@ public class FlywayCleanMigrateRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        flyway.clean(); // Clean the databasejenkins test5
+        flyway.clean(); // Clean the databasejenkins test6
         flyway.migrate(); // Apply migrations
     }
 }
